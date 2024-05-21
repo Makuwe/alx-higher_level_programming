57db4954b34bd1110676640beed97e417a15115c
Makuwe <mmakuwe98@gmail.com>
2024-05-21 16:10:14 -0700

    File

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,8 +1,8 @@
--- Import in hbtn_0c_0 database this table dump
--- a script that displays the top 3 of cities temperature during July and August ordered by temperature (descending)
-SELECT*FROM `temperatures`
-WHERE `month` = 7 OR `month` = 8;
-SELECT `city`, AVG(`value`) AS avg_temp
-FROM `temp_july_aug`
+-- Displays the 3 cities with the highest average
+-- temperatures between July and August.
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
 GROUP BY `city`
-ORDER BY `avg_temp` DESC `limit` 3;
+ORDER BY `avg_temp` DESC
+LIMIT 3;
