2486063d33625a7ad08c0ab4086b08efb2d93dce
Makuwe <mmakuwe98@gmail.com>
2024-07-04 15:13:00 -0700

    tasks

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that was a fun effort in breaking to http protocols
-curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -H "Origin: X-School" -d "user_id=98" 0.0.0.0:5000/catch_me
