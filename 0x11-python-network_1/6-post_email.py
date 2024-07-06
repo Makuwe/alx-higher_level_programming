8f87244b7b3087d7ead3bae1e28130aac0a23371
Makuwe <mmakuwe98@gmail.com>
2024-07-05 18:03:57 -0700

    email post

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
-import sys
 import requests
+from sys import argv
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-
-    r = requests.post(url, data=value)
+    url = argv[1]
+    payload = {'email': argv[2]}
+    r = requests.post(url, data=payload)
     print(r.text)
